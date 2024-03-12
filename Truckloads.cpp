538525025d51e84e96ddffdef1b01c5f23a444b0
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2024-03-12 19:00:17 +1030

    fixed trucks basic

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,12 +1,12 @@
 #include "Truckloads.h"
 
 
-int Truckloads::numTrucks(int numCrates, int maxLoad) {
-        if (numCrates <= maxLoad) {
+int Truckloads::numTrucks(int numCrates, int loadSize) {
+        if (numCrates <= loadSize) {
             return 1;
         }
         if (numCrates % 2) {
-            return numTrucks(numCrates%maxLoad+numCrates/2, maxLoad) + numTrucks(numCrates/2, maxLoad);
+            return numTrucks(numCrates%loadSize+numCrates/2, loadSize) + numTrucks(numCrates/2, loadSize);
         }
-        return 2 * numTrucks(numCrates/2, maxLoad);
+        return 2 * numTrucks(numCrates/2, loadSize);
     }
