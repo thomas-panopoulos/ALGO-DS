0af93d0500ca12a6ac84c08526b77764ba660b90
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2024-03-31 22:46:06 +1030

    test

--- Finder.cpp
+++ Finder.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
 vector<int> result;
+size_t j;
 for(size_t i = 1; i <= s2.size(); i++) {
-if (result.size() > 0) {
-    size_t found = s1.find(s2.substr(0, i), result.at(i-2));
-}
+    
+    size_t found = s1.find(s2.substr(0, i), j);
 
-size_t found = s1.find(s2.substr(0, i),0);
+    size_t found = s1.find(s2.substr(0, i),0);
+    j = found;
 
-
-if (found != string::npos) {
-result.push_back(found);
-} else {
-result.push_back(-1);
-}
-}
-return result;
+    if (found != string::npos) {
+    result.push_back(found);
+    } else {
+    result.push_back(-1);
+    }
+    }
+    return result;
 }
