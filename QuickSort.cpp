d0e2e32e9158d3b495f2464d1a5e3b8285ff76b6
Thomas.Panopoulos <thomas.panopoulos@student.adelaide.edu.au>
2024-04-21 18:18:14 +0930

    works now somehow

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,8 +1,9 @@
 #include "QuickSort.h"
 #include <algorithm>
 #include <vector>
-
+#include <iostream>
 std::vector<int> QuickSort::sort(std::vector<int> list) {
+    int pivotCount =0;
     int pivot;
     if (list.size() <= 1)
         return list;
@@ -12,19 +13,27 @@ std::vector<int> QuickSort::sort(std::vector<int> list) {
     else {pivot = list[list.size() / 2];}
     
     std::vector<int> left, right, result;
-
     for (int i = 0; i < list.size(); i++) {
-        if (list[i] < pivot)
+        if (list[i] < pivot) {
             left.push_back(list[i]);
-        else if (list[i] >= pivot)
+        }
+        else if (list[i] > pivot) {
             right.push_back(list[i]);
+        }
+        else if (list[i] == pivot) {
+            pivotCount++;
+            //std::cout << "test" << std::endl;
+        }
+
     }
 
     left = sort(left);
     right = sort(right);
 
     result.insert(result.end(), left.begin(), left.end());
+    for (int i = 0; i < pivotCount; i++) {
     result.push_back(pivot);
+    }
     result.insert(result.end(), right.begin(), right.end());
 
     return result;
